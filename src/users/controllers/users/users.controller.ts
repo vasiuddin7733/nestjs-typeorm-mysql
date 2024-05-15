54b6ad74087f53122217b5d12b0cf508d5097265
vasiuddin7733 <vasiuddin7733>
2024-05-15 18:40:53 +0200

    add get users query

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    getUsers(){ }
+    
+    @Post()
+    createUser(){}
+}
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/user';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+   constructor(@InjectRepository(User) private userRepository : Repository<User>){}
+    findUsers(){}
+    createUser(){}
+}
